c7bd635ce53eb0ad41280b87bb4f79fb21812e53
José Salazar <jmsalazar.macg@hotmail.com>
2025-02-05 18:17:17 -0600

    Collision detection

--- lib/board.dart
+++ lib/board.dart
@@ -1,8 +1,17 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:tetris_app/piece.dart';
 import 'package:tetris_app/values.dart';
 import 'pixel.dart';
+
+List<List<Tetromino?>> gameBoard = List.generate(
+  colLength,
+  (i) => List.generate(
+    rowLength,
+    (j) => null,
+  ),
+);
 
 class GameBoard extends StatefulWidget {
   const GameBoard({super.key});
@@ -12,10 +21,6 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
-  //grid dimensions
-  int rowLength = 10;
-  int colLength = 15;
-
   Piece currentPiece = Piece(type: Tetromino.T);
 
   @override
@@ -37,10 +42,60 @@ class _GameBoardState extends State<GameBoard> {
       frameRate,
       (timer) {
         setState(() {
+          checkLanding();
           currentPiece.movePiece(Direction.down);
         });
       },
     );
+  }
+
+  bool checkCollision(Direction direction) {
+    for (int i = 0; i < currentPiece.position.length; i++) {
+      //calculate the row and collision
+      int row = (currentPiece.position[i] / rowLength).floor();
+      int col = currentPiece.position[i] % rowLength;
+
+      if (direction == Direction.left) {
+        col -= 1;
+      } else if (direction == Direction.right) {
+        col += 1;
+      } else if (direction == Direction.down) {
+        row += 1;
+      }
+
+      // check if the piece is out of bounds (either too low or too far to the left or right)
+      if (row >= colLength ||
+          col < 0 ||
+          col >= rowLength ||
+          (row >= 0 && gameBoard[row][col] != null)) {
+        return true;
+      }
+    }
+
+    // if no collision detected, return false
+    return false;
+  }
+
+  void checkLanding() {
+    if (checkCollision(Direction.down)) {
+      for (int i = 0; i < currentPiece.position.length; i++) {
+        int row = (currentPiece.position[i] / rowLength).floor();
+        int col = currentPiece.position[i] % rowLength;
+
+        if (row >= 0 && col >= 0) {
+          gameBoard[row][col] = currentPiece.type;
+        }
+      }
+      createNewPiece();
+    }
+  }
+
+  void createNewPiece() {
+    Random rand = Random();
+    Tetromino randomType =
+        Tetromino.values[rand.nextInt(Tetromino.values.length)];
+    currentPiece = Piece(type: randomType);
+    currentPiece.initializePiece();
   }
 
   @override
@@ -53,8 +108,12 @@ class _GameBoardState extends State<GameBoard> {
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: rowLength),
         itemBuilder: (context, index) {
+          int row = (index / rowLength).floor();
+          int col = index % rowLength;
           if (currentPiece.position.contains(index)) {
             return Pixel(color: Colors.yellow, child: index);
+          } else if (gameBoard[row][col] != null) {
+            return Pixel(color: Colors.pink, child: '');
           } else {
             return Pixel(color: Colors.grey[900], child: index);
           }
--- lib/piece.dart
+++ lib/piece.dart
@@ -66,7 +66,6 @@ class Piece {
           -15,
         ];
         break;
-      default:
     }
   }
 
